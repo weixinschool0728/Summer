25a853a99b08f5c78847b927864b4d0e3dcd252c
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2015-10-15 16:40:57 +0800

    first add

---
+++ Summer/summer.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @copyright (c) xiayuanchaun
+ * time 2015-10-15
+ * @author xiayuanchuan<772321344@qq.com>
+ * Description 入口文件
+ */
+    
---
+++ Xiayuanchuan/Webroot/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ *$time 2015-10-15
+ * @author xiayuanchuan<772321344@qq.com>
+ */
+
